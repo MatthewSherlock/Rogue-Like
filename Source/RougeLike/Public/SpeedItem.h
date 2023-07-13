b7ccd56e7798b9279b4010b6c703a44a97d7cba2
MatthewSherlock <matthew.sherlock01@gmail.com>
2023-07-13 23:36:16 +0100

    Init

---
+++ Source/RougeLike/Public/SpeedItem.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item.h"
+#include "SpeedItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROUGELIKE_API ASpeedItem : public AItem
+{
+	GENERATED_BODY()
+
+		virtual void UseItem() override;
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Info")
+		float speedMultiplier;
+};
